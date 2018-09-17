0c52f8d2cd084512bed725364aff36429525c396
antditto <anthonyditomasso@gmail.com>
2018-09-16 20:09:06 -0400

    Fixed parse

--- services/keyword-parser.js
+++ services/keyword-parser.js
@@ -1,16 +1,28 @@
 module.exports = function(keywordString) {
   //running match test
-  const splitted = keywordString.match(/(['"])((?:\\\1|.)+?)\1|([^\s,"']+)/g);
-  //const splitted = keywordString.match(/\w+|["'](?:\\["']|[^"'])+['"]/g);
+  const specialtyEvaluation = [',','.','?','!','&']
+  //const splitted = keywordString.match(/(['"])((?:\\\1|.)+?)\1|([^\s,"']+)/g);
+  const splitted = keywordString.match(/\w+|["'](?:\\["']|[^"'])+['"]/g);
   //sort to handle keywords within keywords correctly
   splitted.sort((a, b) => b.length - a.length)
   //replacing quotes with empty string for evaluation
+  let spaceLocation = 0;
+  let pipeLocation = 0;
   for (let i = 0; i < splitted.length; i++) {
-      splitted[i] = splitted[i].replace(/['"]+/g, '')
-  //creating copy of keyword for newline check
-      if (splitted[i].indexOf(' ') != -1) {
-        splitted.push(splitted[i].replace(' ', '|' ))
-      }
+    splitted[i] = splitted[i].replace(/['"]+/g, '')
+    //creating copy of keyword for newline check
+    if (splitted[i].indexOf('|') != -1 && splitted[i].indexOf('|') < spaceLocation) {
+      splitted.push(splitted[i].replace('|', ' '))
+    }
+    if (splitted[i].indexOf(' ', spaceLocation) != -1 && splitted[i].indexOf(' ', spaceLocation) > spaceLocation) {
+      spaceLocation = splitted[i].indexOf(' ', spaceLocation);
+      splitted.push(splitted[i].replace(' ', '|'))
+    }
+    specialtyEvaluation.forEach((character) => {
+      if (splitted[i].endsWith(character)) {
+        splitted[i] += ' ';
+      };
+    });
   }
   return splitted;
 }
